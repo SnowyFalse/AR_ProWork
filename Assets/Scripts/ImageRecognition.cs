2a3c7ac3b3f07846e65a508975e69aa258dda998
Theresa <theresa.dietinger@gmail.com>
2023-05-02 16:20:19 +0200

    pls work

---
+++ Assets/Scripts/ImageRecognition.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.ARFoundation;
+
+public class ImageRecognition : MonoBehaviour
+{
+
+    private ARTrackedImageManager aRTrackedImageManager;
+
+    private void Awake()
+    {
+        aRTrackedImageManager = FindObjectOfType<ARTrackedImageManager>();
+    }
+
+    public void OnEnable()
+    {
+        aRTrackedImageManager.trackedImagesChanged += OnImageChanged;
+    }
+
+    public void OnDisable()
+    {
+        aRTrackedImageManager.trackedImagesChanged -= OnImageChanged;
+    }
+
+    public void OnImageChanged(ARTrackedImagesChangedEventArgs args)
+    {
+        foreach(var trackedImage in args.added)
+        {
+            Debug.Log(trackedImage.name);
+        }
+    }
+
+}
